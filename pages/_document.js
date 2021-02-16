860554cd2ff7791ec01eb4c809f48ee2553cda4d
EDusik <eduardodusik@gmail.com>
2021-02-16 16:12:33 -0300

    refactor: changing keywords in 'package.json' and '_document.js' files

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
 					<meta charset="UTF-8" />
 					<meta name="language" content="PT" />
 
-					<title>Eduardo Dusik - Front-End Developer</title>
+					<title>Eduardo Dusik - Desenvolvedor Front-End</title>
 					<link rel="shortcut icon" href="/static/favicon.ico" />
 					<link rel="manifest" href="/static/manifest.json" />
 					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" />
@@ -19,15 +19,14 @@ export default class MyDocument extends Document {
 					<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1 user-scalable=no" />
 					<meta name="author" content="Eduardo Dusik" />
 					<meta name="title" content="Eduardo Dusik - Portfolio" />
-					<meta name="description" content="Eduardo Dusik - Front-End Developer" />
-					<meta name="keywords" content="eduardo, dusik, desenvolvedor, developer, front-end, portfolio, github" />
-					<meta name="robots" content="index, follow" />
+					<meta name="description" content="Eduardo Dusik - Desenvolvedor Front-End" />
+					<meta name="keywords" content="eduardo, dusik, desenvolvedor, front-end, portfolio, dev" />
 
 					<meta property="og:type" content="website" />
 					<meta property="og:title" content="Eduardo Dusik - Portfolio" />
-					<meta property="og:description" content="Eduardo Dusik - Front-End Developer" />
+					<meta property="og:description" content="Eduardo Dusik - Desenvolvedor Front-End" />
 					<meta property="og:image" content="/static/logo192.png" />
-					<meta property="og:site_name" content="Eduardo Dusik - Front-End Developer" />
+					<meta property="og:site_name" content="Eduardo Dusik - Desenvolvedor Front-End" />
 				</Head>
 				<body>
 					<Main />
