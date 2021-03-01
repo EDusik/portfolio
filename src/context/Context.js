2682132d73069fd1fca282b9323ebf54ae4c8b0a
EDusik <eduardodusik@gmail.com>
2021-02-28 22:00:18 -0300

    refactor: removing search from Context api

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,29 +1,23 @@
 import { createContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 export const Context = createContext({});
 
 export const ContextProvider = ({ children, ...rest }) => {
 	const [profile, setProfile] = useState(rest.profile ?? {});
-	const [repositories, setRepositories] = useState(rest.repositories ?? {});
-	const [search, setSearch] = useState(rest.search ?? "");
+	const [repositories, setRepositories] = useState(rest.repositories ?? []);
 	const [isLoading, setIsLoading] = useState(rest.isLoading ?? true);
 	const [hasError, setHasError] = useState(rest.hasError ?? false);
-
-	useEffect(() => {
-		sessionStorage.setItem("profile", JSON.stringify(profile));
-	}, [profile]);
 
 	return (
 		<Context.Provider
 			value={{
 				profile,
 				repositories,
-				search,
 				isLoading,
 				hasError,
 				setProfile,
 				setRepositories,
-				setSearch,
 				setIsLoading,
 				setHasError
 			}}
